651819989dbee497067b2b1206e0bb85fbc38447
brad schrag <brad.schrag@gmail.com>
2019-01-30 10:52:17 -0600

    adds view tests/new

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,15 @@ class TestsController < ApplicationController
 
 	get '/tests' do
 		@tests = Tests.all
-		
+
 		erb :'/tests/index'
+	end
+
+	get '/tests/new' do
+		erb :'/tests/new'
+	end
+
+	post '/tests' do
+
 	end
 end
